aa9a5a350c3f3cce3299cb8457abe144c24562dd
NejcZun <NejcZun@noreply.github.com>
2019-03-09 16:21:51 +0100

    Update signup.php

--- signup.php
+++ signup.php
@@ -125,7 +125,7 @@
     <div class="container-fluid clearfix">
         <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © 2019
 			<a href="https://www.fri.uni-lj.si/sl" target="_blank" style="color:#5983e8;">FRI Ljubljana</a>. All rights reserved.</span>
-			<span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Avtorja: Nejc Žun, Grega Černak
+			<span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Avtorja: Nejc Žun, Gregor Černak
 		</span>
     </div>
   </footer>
