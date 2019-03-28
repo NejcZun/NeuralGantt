b59e1115c85768f27587633bed70a1e430384526
GREGOR ČERNAK <gc4703@student.uni-lj.si>
2019-03-28 13:58:45 +0100

    Register PHP
    SALT ISSUE resolved,
    login validation w/ php & html alerts
    edited file so script is in
    vendors/js/register.js
    php validation in auth.php

--- vendors/functions/db_mysql.php
+++ vendors/functions/db_mysql.php
@@ -33,6 +33,19 @@ function db_user_exists($user){
     if($result['checkUser'] == 1) return true;
     return false;
 }
+
+function db_email_exists($email){
+    global $db;
+
+    $query = "SELECT EXISTS(SELECT id from user WHERE email = :email) AS checkEmail";
+    $stmt = $db->prepare($query);
+    $stmt->bindParam(":email", $email);
+    $stmt->execute();
+    $result = $stmt->fetch();
+    if($result['checkEmail'] == 1) return true;
+    return false;
+}
+
 function db_user_exists_byId($id){
     global $db;
 
@@ -119,6 +132,29 @@ function db_user_login($user, $pass){
     $result = $stmt->fetch();
     if($result['checkLogin'] == 1) return true;
     return false;
+}
+
+function generate_salt(){
+    // temporary salt
+    return bin2hex(random_bytes(20));
+}
+
+function db_user_register($user, $pass, $fname, $lname, $email){
+    global $db;
+
+    $salt = generate_salt();
+    $pass = hash_pbkdf2('sha3-256', $pass, $salt, 3);
+    $role_id = "3";
+    $query = "INSERT INTO user (email, uname, password, salt, fname, lname, role_id) VALUES (:email, :uname, :pass, :salt, :fname, :lname, :role)";
+    $stmt = $db->prepare($query);
+    $stmt->bindParam(":email", $email);
+    $stmt->bindParam(":uname", $user);
+    $stmt->bindParam(":pass", $pass);
+    $stmt->bindParam(":salt", $salt);
+    $stmt->bindParam(":fname", $fname);
+    $stmt->bindParam(":lname", $lname);
+    $stmt->bindParam(":role", $role_id);
+    $stmt->execute();
 }
 function cookie_get_username(){
 	return base64_decode($_COOKIE['user']);
